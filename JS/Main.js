de2784285aad6c59f86d0dd2bdbdbb70c461afdf
Zana Amanallhy <zanaamanallhy80@gmail.com>
2023-05-26 15:07:13 +0430

    Adding a CDN to the typing section along with its JS coding.

---
+++ JS/Main.js
@@ -0,0 +1,7 @@
+/* =============== typing animation =============== */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developer", "Graphic Designer", "YouTuber", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
